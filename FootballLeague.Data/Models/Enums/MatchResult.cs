eb9d37b051733f1d512f8101163e5d377d79c8bb
Nikolay GeorgievN <nickwork5500@gmail.com>
2022-07-02 20:47:38 +0300

    Add MatchResult and MatchScore enums

---
+++ FootballLeague.Data/Models/Enums/MatchResult.cs
@@ -0,0 +1,9 @@
+﻿namespace FootballLeague.Data.Models.Enums
+{
+    public enum MatchResult
+    {
+        Win = 1,
+        Loss = 2,
+        Draw = 3
+    }
+}
---
+++ FootballLeague.Data/Models/Enums/MatchScore.cs
@@ -0,0 +1,9 @@
+﻿namespace FootballLeague.Data.Models.Enums
+{
+    public enum MatchScore
+    {
+        Loss = 0,
+        Draw = 1,
+        Win = 3,
+    }
+}
