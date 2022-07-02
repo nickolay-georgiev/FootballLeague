1824bad93cf3df77158a80a12432dfd0e3b5d06d
Nikolay GeorgievN <nickwork5500@gmail.com>
2022-07-02 20:47:16 +0300

    Add PlayerPosition enumeration

---
+++ FootballLeague.Data/Models/Enums/PlayerPosition.cs
@@ -0,0 +1,17 @@
+﻿namespace FootballLeague.Data.Models.Enums
+{
+    public enum PlayerPosition
+    {
+        Goalkeeper = 1,
+        RightFullback = 2,
+        LeftFullback = 3,
+        CenterBack = 4,
+        Sweeper = 5,
+        Defender = 6,
+        RightMidfielder = 7,
+        CentralBoxToBoxMidfielder = 8,
+        Striker = 9,
+        AttackingMidfielder = 10,
+        LeftMidfielder = 11
+    }
+}
